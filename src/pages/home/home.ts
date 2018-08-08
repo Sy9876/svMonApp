1e3e4b21c00621543105264d5474a4e583dd2502
shenyue <sheny@lzt.com.cn>
2018-08-08 13:49:25 +0800

    add http request

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public http: HttpClient ) {
 
   }
 
+  msg:string;
+
+  myClick():void {
+    console.log('haha');
+
+    this.msg='request';
+    this.http.get('/api/v1/auth/menus1.json')
+    .subscribe((res: Response) => {
+      console.log('res: ', res);
+      // console.log(res);
+      this.msg=res.status.toString();
+    },
+    (err: any) => {
+      console.log('error: ', err);
+      this.msg=err.status;
+    });
+
+  }
 }
